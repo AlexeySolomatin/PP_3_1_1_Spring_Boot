b481b5edc9e0414d3fcf4a427d461b496f0ecfd2
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2022-10-10 19:42:27 +0300

    Исправил

--- src/main/java/ru/solomatin/preproject/springboot/service/UserService.java
+++ src/main/java/ru/solomatin/preproject/springboot/service/UserService.java
@@ -5,11 +5,9 @@ import ru.solomatin.preproject.springboot.model.User;
 
 import java.util.List;
 
-@Transactional
+
 public interface UserService {
-    @Transactional(readOnly = true)
     public List<User> getAllUsers();
-    @Transactional(readOnly = true)
     public User getUser(int id);
     public void save(User user);
     public void update(int id, User updateUser);
--- src/main/java/ru/solomatin/preproject/springboot/service/UserServiceImpl.java
+++ src/main/java/ru/solomatin/preproject/springboot/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.solomatin.preproject.springboot.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.solomatin.preproject.springboot.dao.UserDAO;
 import ru.solomatin.preproject.springboot.model.User;
 
@@ -15,26 +16,31 @@ public class UserServiceImpl implements UserService {
         this.userDAO = userDAO;
     }
     @Override
+    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         return userDAO.getAllUsers();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUser(int id) {
         return userDAO.getUser(id);
     }
 
     @Override
+    @Transactional
     public void save(User user) {
         userDAO.save(user);
     }
 
     @Override
+    @Transactional
     public void update(int id, User updateUser) {
         userDAO.update(id, updateUser);
     }
 
     @Override
+    @Transactional
     public void delete(int id) {
         userDAO.delete(id);
     }
